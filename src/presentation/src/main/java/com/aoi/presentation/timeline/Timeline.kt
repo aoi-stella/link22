cb99ed9374917dcf3484b27701f5fd945d28d846
aoi-stella <aoi.stella.dev@gmail.com>
2024-02-23 18:22:27 +0900

    feat: add dummy items

--- src/presentation/src/main/java/com/aoi/presentation/timeline/Timeline.kt
+++ src/presentation/src/main/java/com/aoi/presentation/timeline/Timeline.kt
@@ -3,13 +3,26 @@ package com.aoi.presentation.timeline
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.aoi.presentation.timeline.ui.theme.Link22Theme
 
 class Timeline : ComponentActivity() {
@@ -31,11 +44,57 @@ class Timeline : ComponentActivity() {
 
 @Composable
 fun Timeline(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(25.dp),
+    ) {
+        items(10) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = "Image Area",
+                    fontSize = 24.sp,
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .fillMaxWidth()
+                        .background(Color.Gray)
+                )
+                Text(
+                    text = "Albabat, Kasseika, Kuiper: New Ransomware Gangs Rise with Rust and Golang",
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    fontSize = 24.sp,
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                )
+                Divider(
+                    modifier = Modifier
+                        .height(2.5.dp)
+                        .fillParentMaxWidth(.5f)
+                        .background(Color.Gray)
+                        .align(Alignment.CenterHorizontally)
+                        .padding(vertical = 10.dp) // 上下の余白
+                )
+                Text(
+                    text = "Albabat, Kasseika, Kuiper: New Ransomware Gangs Rise with Rust and Golang",
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    fontSize = 20.sp,
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                )
+                Text(
+                    text = "Post: 1/31/2024",
+                    fontSize = 16.sp,
+                    modifier = Modifier
+                        .align(Alignment.End)
+                )
+            }
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
