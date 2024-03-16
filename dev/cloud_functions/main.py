561e596a86e8e1facb18a7c8a25ea2c52f6180d5
aoi-stella <aoi.stella.dev@gmail.com>
2024-03-16 20:57:14 +0900

    modify: modify main

--- dev/cloud_functions/main.py
+++ dev/cloud_functions/main.py
@@ -1,54 +1,10 @@
-import feedparser
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-
+from article.ArticleRepository import ArticleRepository
+from firestore.firestore_api import FirestoreAPI
+from article.ArticleDataClass import Article
 
 url = "https://feeds.feedburner.com/TheHackersNews"
 
-class Article:
-   """記事クラス
-   """
-   def __init__(self, title: str, link: str, summary: str):
-      """コンストラクタ
-
-      Args:
-         title (str): 記事のタイトル
-         link (str): 記事のリンク
-         summary (str): 記事の概要
-      """
-      self.title = title
-      self.link = link
-      self.summary = summary
-
-def get_article(url: str) -> list[Article]:
-   """指定されたURLのRSSフィードから記事を取得する
-
-   Args:
-       url (str): RSSフィード取得先のURL
-
-   Returns:
-       list[Article]: 記事のリスト
-   """
-   articles = []
-   # 記事を取得
-   feeds = feedparser.parse(url)
-   for article in feeds['entries']:
-      title = article['title']
-      link = article['link']
-      summary = article['summary']
-      
-      # 記事をリストへ追加する
-      articles.append(Article(title, link, summary))
-   return articles
-   
 # エントリーポイント
 if __name__ == "__main__":
-   cred = credentials.Certificate("../key/firebase-key.json")
-   firebase_admin.initialize_app(cred)
-   articles = get_article(url)
-   print(articles[0].title)
-   db = firestore.client()
-   docs = db.collection('users').get()
-   for doc in docs:
-      print(doc.to_dict())
+   article_repository = ArticleRepository(FirestoreAPI("../key/firebase-key.json"))
+   article_repository.save(Article("title", "link", "summary"))
