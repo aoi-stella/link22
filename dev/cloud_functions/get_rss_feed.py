771852b18c39865c8899add37cd43e053fb82a09
aoi-stella <aoi.stella.dev@gmail.com>
2024-03-16 19:54:32 +0900

    feat: add rss feed code

--- dev/cloud_functions/get_rss_feed.py
+++ dev/cloud_functions/get_rss_feed.py
@@ -1,13 +1,45 @@
 import feedparser
 import pprint
 
-url = "https://hnrss.org/newest"
+url = "https://feeds.feedburner.com/TheHackersNews"
 
-f = feedparser.parse(url)
+class Article:
+   """記事クラス
+   """
+   def __init__(self, title: str, link: str, summary: str):
+      """コンストラクタ
 
-for article in f['entries']:
-   print('title is ' + article['title'] + '\n')
-   print('link is ' + article['link'] + '\n')
-   print('summary is ' + article['summary'] + '\n')
-   print('\n')
-   exit()
+      Args:
+         title (str): 記事のタイトル
+         link (str): 記事のリンク
+         summary (str): 記事の概要
+      """
+      self.title = title
+      self.link = link
+      self.summary = summary
+
+def get_article(url: str) -> list[Article]:
+   """指定されたURLのRSSフィードから記事を取得する
+
+   Args:
+       url (str): RSSフィード取得先のURL
+
+   Returns:
+       list[Article]: 記事のリスト
+   """
+   articles = []
+   # 記事を取得
+   feeds = feedparser.parse(url)
+   for article in feeds['entries']:
+      title = article['title']
+      link = article['link']
+      summary = article['summary']
+      
+      # 記事をリストへ追加する
+      articles.append(Article(title, link, summary))
+   return articles
+   
+# エントリーポイント
+if __name__ == "__main__":
+   articles = get_article(url)
+   print(articles[0].title)
