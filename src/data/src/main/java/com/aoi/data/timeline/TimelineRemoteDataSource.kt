8a7d760eef51941c362af8d76c064a93926ac5b4
aoi-stella <aoi.stella.dev@gmail.com>
2024-03-17 23:29:56 +0900

    feat: add remote data source

--- src/data/src/main/java/com/aoi/data/timeline/TimelineRemoteDataSource.kt
+++ src/data/src/main/java/com/aoi/data/timeline/TimelineRemoteDataSource.kt
@@ -1,4 +1,44 @@
 package com.aoi.data.timeline
 
-class TimelineRemoteDataSource {
+import com.aoi.data.api.firebase.FirebaseAPI
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+/**
+ * Timeline画面用のリモートデータソースクラス
+ *
+ * @param firebaseApi FirebaseAPI
+ */
+class TimelineRemoteDataSource(private val firebaseApi: FirebaseAPI) {
+
+    companion object{
+        // Firebaseのコレクション名
+        private const val FIREBASE_ARTICLE_COLLECTION_NAME = "articles"
+        // Firebaseのドキュメントのキー[タイトル]
+        private const val FIREBASE_ARTICLE_TITLE_KEY = "title"
+        // Firebaseのドキュメントのキー[概要]
+        private const val FIREBASE_ARTICLE_SUMMARY_KEY = "summary"
+        // Firebaseのドキュメントのキー[URL]
+        private const val FIREBASE_ARTICLE_URL_KEY = "url"
+    }
+
+    /**
+     * Firebaseから記事一覧を取得する
+     *
+     * @return Result<List<TimelineDataClass>> Firebaseから取得した記事一覧
+     */
+    suspend fun getArticlesFromFirebase(): Result<List<TimelineDataClass>> = suspendCoroutine { continuation ->
+        firebaseApi.readAllDocument(FIREBASE_ARTICLE_COLLECTION_NAME) { result ->
+            val mappedResult = result.mapCatching { documents ->
+                documents.map { document ->
+                    TimelineDataClass(
+                        title = document[FIREBASE_ARTICLE_TITLE_KEY] as? String ?: "",
+                        summary = document[FIREBASE_ARTICLE_SUMMARY_KEY] as? String ?: "",
+                        url = document[FIREBASE_ARTICLE_URL_KEY] as? String ?: ""
+                    )
+                }
+            }
+            continuation.resume(mappedResult)
+        }
+    }
 }
--- src/data/src/main/java/com/aoi/data/timeline/TimelineRepository.kt
+++ src/data/src/main/java/com/aoi/data/timeline/TimelineRepository.kt
@@ -1,4 +1,24 @@
 package com.aoi.data.timeline
 
-class TimelineRepository {
+/**
+ * Timeline画面用のリポジトリ
+ *
+ * @property timelineLocalDataSource ローカルデータソース
+ * @property timelineRemoteDataSource リモートデータソース
+ */
+class TimelineRepository(
+    private val timelineLocalDataSource: TimelineLocalDataSource,
+    private val timelineRemoteDataSource: TimelineRemoteDataSource
+) {
+
+    /**
+     * 記事を取得する
+     *
+     * @return Result<List<TimelineDataClass>> 記事のリスト
+     *
+     * TODO: オフライン時やキャッシュ時の挙動を考慮すること。
+     */
+    suspend fun getArticles(): Result<List<TimelineDataClass>> {
+        return timelineRemoteDataSource.getArticlesFromFirebase()
+    }
 }
