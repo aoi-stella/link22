eb76e928357e2bf998af6ba694734c7dbc46c1e4
aoi-stella <aoi.stella.dev@gmail.com>
2024-04-07 18:39:17 +0900

    modify: update firebase ver

--- src/presentation/build.gradle.kts
+++ src/presentation/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation(project(":data"))
-    implementation("com.google.firebase:firebase-common-ktx:20.4.2")
+    implementation("com.google.firebase:firebase-common-ktx:20.4.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
