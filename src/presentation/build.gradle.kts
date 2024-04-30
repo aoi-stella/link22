7c025a9182b6f7a2300ba8d13325985e71ace031
aoi-stella <aoi.stella.dev@gmail.com>
2024-04-30 23:13:23 +0900

    feat: add coil lib to presentation module

--- src/presentation/build.gradle.kts
+++ src/presentation/build.gradle.kts
@@ -81,6 +81,9 @@ dependencies {
     testImplementation("com.google.dagger:hilt-android-testing:2.51.1")
     testAnnotationProcessor("com.google.dagger:hilt-compiler:2.51.1")
 
+    // Coil
+    implementation("io.coil-kt:coil-compose:2.6.0")
+
     // Multi Module
     implementation(project(":util"))
     implementation(project(":domain"))
